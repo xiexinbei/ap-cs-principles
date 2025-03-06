f9c2b5f33930e986cac2521b8401bb0ea6c35de9
xinbei <xinbei0706@gmail.com>
2025-03-06 11:25:18 -0500

    ap-sc-principles

--- getcharacter.py
+++ getcharacter.py
@@ -7,3 +7,31 @@ class GetCharacter:
             'kuromi': kuromi
         }
 
+
+    def add(self, apple_home: str) -> None:
+        if apple_home == 'hellokitty':
+            self.apple_home['hellokitty'] = self.apple_home.get('hellokitty') + 1
+        if apple_home == 'melody':
+            self.apple_home['melody'] = self.apple_home.get('melody') + 1
+        if apple_home == 'hangyodon':
+            self.apple_home['hangyodon'] = self.apple_home.get('hangyodon') + 1
+        if apple_home == 'kuromi':
+            self.apple_home['kuromi'] = self.apple_home.get('kuromi') + 1
+
+    def sort(self) -> str:
+        score = 0
+        result = ''
+        for house, points in self.apple_home.items():
+            if points > score:
+                result=house
+
+        return result
+
+    def clear(self) -> None:
+        self.house = {
+            'hellokitty': 0,
+            'melody': 0,
+            'hangyodon': 0,
+            'kuromi': 0
+        }
+    

c497a7a8d08e41229de435abe509bd59ed11c89e
xinbei <xinbei0706@gmail.com>
2025-03-12 10:04:18 -0400

    ap-comsci-p

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request 
 
 app = Flask(__name__)
 
@@ -13,5 +13,9 @@ def kuromi_image():
 @app.route('/question/1', methods = ['GET', 'POST'])
 def first_question():
     answers = ['red', 'pink', 'blue', 'black']
+
+    if request.method == 'GET':
+        return render_template('question_1.html', answers = answers)
+
 if __name__ =='__main__':
     app.run(host='127.0.0.1')
